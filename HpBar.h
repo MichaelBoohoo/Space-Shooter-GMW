367d01799fe076e253e24f004c9259a009311473
MichaelBoohoo <168357167+MichaelBoohoo@users.noreply.github.com>
2026-01-18 18:00:59 +0100

    Delete HpBar.h

--- HpBar.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-inline sf::RectangleShape createBar(const sf::Color& color) {
-    sf::RectangleShape bar({50,5});
-    bar.setFillColor(color);
-    return bar;
-}
